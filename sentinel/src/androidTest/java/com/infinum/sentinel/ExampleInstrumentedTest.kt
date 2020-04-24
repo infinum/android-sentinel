3d7eac5229f0772274ad69af95151de68454c680
Bojan Komljenović <bojan.komljenovic@infinum.hr>
2020-04-24 23:27:39 +0200

    Convert to lazy init

--- sentinel/src/androidTest/java/com/infinum/sentinel/ExampleInstrumentedTest.kt
+++ sentinel/src/androidTest/java/com/infinum/sentinel/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package com.infinum.sentinel
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +13,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
