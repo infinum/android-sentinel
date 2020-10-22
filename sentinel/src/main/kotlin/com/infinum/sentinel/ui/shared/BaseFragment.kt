c1b108de71a0bfaecc044c059e031be7b1e90932
Bojan Komljenović <bojan.komljenovic@infinum.hr>
2020-10-22 20:58:49 +0200

    Replace BottomSheetDialogFragment with DialogFragment. Remove ContentProvider

--- sentinel/src/main/kotlin/com/infinum/sentinel/ui/shared/BaseFragment.kt
+++ sentinel/src/main/kotlin/com/infinum/sentinel/ui/shared/BaseFragment.kt
@@ -1,27 +1,36 @@
 package com.infinum.sentinel.ui.shared
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.annotation.RestrictTo
+import androidx.fragment.app.DialogFragment
 import androidx.viewbinding.ViewBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.infinum.sentinel.R
+
 
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 internal abstract class BaseFragment(
     @LayoutRes private val contentLayoutId: Int
-) : BottomSheetDialogFragment() {
+) : DialogFragment() {
 
     abstract val binding: ViewBinding
 
-    override fun getTheme(): Int = R.style.Sentinel_Theme_BottomSheet
+    override fun getTheme(): Int = R.style.Sentinel_Theme_Dialog
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): BottomSheetDialog =
-        BottomSheetDialog(requireContext(), theme)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        Dialog(requireContext(), theme).also {
+            val window: Window? = it.window
+            val layoutParams: WindowManager.LayoutParams? = window?.attributes
+            layoutParams?.gravity = Gravity.BOTTOM
+            window?.attributes = layoutParams
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
